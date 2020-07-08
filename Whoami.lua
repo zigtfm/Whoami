19629d0cee7ae6cbe992cb57194851db1a0e206b
zigtfm <52834004+zigtfm@users.noreply.github.com>
2020-07-08 22:08:59 +0500

    update translation
    fix

--- Whoami.lua
+++ Whoami.lua
@@ -1,22 +1,22 @@
 --[[ Translation ]]
-translation = {
-	en = {
-		journal = "Journal",
-		notes = "Notes",
-		hidden = "hidden",
-		error = {
-			longMessage = "Too long message",
-		},
-		command = { 
-			nextTurn = "Next turn",
-			allWordsGiven = "All words given.",
-			newGame = "New game",
-			playerWon = "Player %s won",
-		},
-		help = [[<a href='event:update_journal'><r>Version %s</r>
-<p align='center'><font size='14'>Game "Who am i"</font></p>	Every player has a word, which he should guess, to win. The word is given by other players before the game and visible in the journal. (Player, which word is being discussed, can't open the journal, so he cant see the conversation )
-	After all words has been given, all players are asking 3 question by order. Answers can be only "yes" or "no". In the end player can try to guess a word, by writting in chat.
-	After 3rd turn you can ask a tip from choosen by you player, but you can ask 3 questions this turn.
+	translation = {
+		en = {
+			journal = "Journal",
+			notes = "Notes",
+			hidden = "hidden",
+			error = {
+				longMessage = "Too long message",
+			},
+			command = { 
+				nextTurn = "Next turn",
+				allWordsGiven = "All words have been given.",
+				newGame = "New game",
+				playerWon = "Player %s won",
+			},
+			help = [[<a href='event:update_journal'><r>Version %s</r>
+<p align='center'><font size='14'>Game "Who am i"</font></p>    Every player has a word which he needs to guess in order to win. The word is given by other players before the game and is visible in the journal. (Player, whose word is being discussed, can't open the journal, so he can't see the conversation )
+After all words has been given, all players ask 3 questions by order. Answers can be only "yes" or "no". In the end player can try to guess a word by writting in chat.
+After 3rd turn you can ask a tip from chosen by you player, but you can ask 3 questions this turn.
 <p align='center'><font size='14'>Commands</font></p><b>P</b> - join the game
 <b>!word <i>word</i></b> - change the word.
 <b>!</b> - write a message in the journal
@@ -25,22 +25,34 @@ translation = {
 <b>N</b> - add a note in the journal (only you can see it)
 <b>V</b> - vote for the word
 
-Press on the grey square or key <b>J</b> to open the journal]],
+Press on the grey square or key <b>J</b> to open the journal]]
 	},
-	ru = {
-		journal = "Журнал",
-		notes = "Записки",
-		hidden = "скрыто",
-		error = {
-			longMessage = "Слишком длинное сообщение",
-		},
-		command = { 
-			nextTurn = "Следующий ход",
-			allWordsGiven = "Все слова разданы",
-			newGame = "Новая игра",
-			playerWon = "Игрок %s победил",
-		},
-		help = [[<a href='event:update_journal'><r>Версия %s</r>
+		fr = {},
+		br = {},
+		ez = {},
+		tr = {},
+		pl = {},
+		hu = {},
+		ro = {},
+		ar = {},
+		vk = {},
+		nl = {},
+		id = {},
+		de = {},
+		ru = {
+			journal = "Журнал",
+			notes = "Записки",
+			hidden = "скрыто",
+			error = {
+				longMessage = "Слишком длинное сообщение",
+			},
+			command = { 
+				nextTurn = "Следующий ход",
+				allWordsGiven = "Все слова разданы",
+				newGame = "Новая игра",
+				playerWon = "Игрок %s победил",
+			},
+			help = [[<a href='event:update_journal'><r>Версия %s</r>
 <p align='center'><font size='14'>Игра "Кто я"</font></p>	У каждого игрока есть слово, которую ему надо угадать, чтобы победить. Слово задается другими игроками перед игрой и отображается в журнале. (Игрок, чью слово сейчас выбирают, не может открыть журнал, чтобы не видеть обсуждения )
 	Когда все слова разданы, все игроки по очереди задают 3 вопроса. Ответом на них должны быть "да" или "нет". В конце хода игрок может попобовать угадать слово, написав в чат.
 	С после вашего 3-го хода можно попросить подсказку от выбранного вами игрока, но вы не можете задавать вопросы на этот ход.
@@ -53,8 +65,21 @@ Press on the grey square or key <b>J</b> to open the journal]],
 <b>V</b> - голосовать за выбор слова
 
 Нажмите в области серого квадрата или клавишу <b>J</b> чтобы открыть журнал]],
+		},
+		cn = {},
+		ph = {},
+		lt = {},
+		jp = {},
+		fi = {},
+		il = {},
+		it = {},
+		cz = {},
+		hr = {},
+		bg = {},
+		lv = {},
+		ee = {},
+		rs = {},
 	}
-}
 --[[ / ]]
 
 --[[ Who used ]]
@@ -86,9 +111,9 @@ end
 	}
 
 	local community = tfm.get.room.playerList[who_used].community
-	local community = "en"
-	translation = table.copy(translation[community])
+	community = translation[community].journal and community or "en"
 
+	translation = table.copy(translation[community])
 --
 
 --[[ Misc functions --]]
